6ecec4f92ef49c78af5677952665c17d8f70a6b5
chrscchrn <cochranschris@gmail.com>
2020-11-23 20:29:46 -0600

    choosing tech

--- pages/index.js
+++ pages/index.js
@@ -3,63 +3,46 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
+    <>
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Blogify</title>
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Words
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+          <p>
+            post: <br/>
+          date, author, subtitle, body, pictures, on the top there can be a back component
+            <hr/>
+            front: <br/>
+            Cool pic or item for Nav  ||| link back to the portfolio
+            <hr/>
+            title or some piece of words, pictures, or anything for the most intro spot
+            <hr/>
+            have a card for each article
+            <hr/>
+            maybe have another item or something at the bottom to make it looks artsy fartsy
+            <hr/>
+            My personal links
+          </p>
+        
       </main>
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://portfolio-cnc.herokuapp.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          Christopher Cochran{' '}
+          {/* <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} /> */}
         </a>
       </footer>
     </div>
+    </>
   )
 }
