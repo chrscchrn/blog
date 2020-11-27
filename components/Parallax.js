a02d22557b4b632cc926e84d484e362081f25cd6
chrscchrn <cochranschris@gmail.com>
2020-11-27 10:56:39 -0600

    scroll transform done

--- components/Parallax.js
+++ components/Parallax.js
@@ -1,31 +1,22 @@
 import React from 'react';
 import { makeStyles } from "@material-ui/core/styles";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+import styles from "../public/assets/jss/parallaxStyle.js";
 
-const useStyles = makeStyles({
-    parallax: {
-        maxHeight: "1000px",
-        overflow: "hidden",
-        position: "relative",
-        backgroundPosition: "center center",
-        backgroundSize: "cover",
-        margin: "0",
-        padding: "0",
-        border: "0",
-        display: "flex",
-        alignItems: "center",
-        height: "45vw",
-        width: "100%",
-    },
-});
+const useStyles = makeStyles(styles);
 
 export default function Parallax(props) {
     
     let windowScrollTop;
-    // if (window.innerWidth >= 768) {
-    //   windowScrollTop = window.pageYOffset / 3;
-    // } else {
-    //   windowScrollTop = 0;
-    // }
+    React.useEffect(() => {
+        if (window.innerWidth >= 768) {
+            windowScrollTop = window.pageYOffset / 3;
+        } else {
+            windowScrollTop = 0;
+        }
+    }, []);
+    
     
     const [transform, setTransform] = React.useState(
       "translate3d(0," + windowScrollTop + "px,0)"
@@ -46,15 +37,29 @@ export default function Parallax(props) {
         var windowScrollTop = window.pageYOffset / 3;
         setTransform("translate3d(0," + windowScrollTop + "px,0)");
     };
-    
+    const { children, image, filter, small, className } = props;
     const classes = useStyles();
+    const parallaxClasses = classNames({
+        [classes.parallax]: true,
+        [classes.filter]: filter,
+        [classes.small]: small,
+        [className]: className !== undefined,
+    });
 
     return (
         <div
-            className={classes.parallax}
-            style={{backgroundImage: "url(" + props.image + ")"}}
+            className={parallaxClasses}
+            style={{
+                backgroundImage: "url(" + image + ")",
+                transform: transform,
+            }}
         >
-            
+            {children}
         </div>
     )
 }
+
+Parallax.propTypes = {
+    image: PropTypes.string,
+    children: PropTypes.node,
+};
--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import classNames from "classnames";
 import CssBaseline from '@material-ui/core/CssBaseline';
 
 import styles from '../public/assets/jss/Front';
+import { Container } from '@material-ui/core';
 
 const useStyles = makeStyles(styles);
 
@@ -19,29 +20,35 @@ export default function Home() {
 
       <Parallax image={"../assets/img/mount.jpg"} >
         {/* grid container and item, some titles n shit */}
+        <div className={classes.container}>
+          Jim
+        </div>
       </Parallax>
       <div className={classNames(classes.main, classes.mainRaised)}> 
-        <h1>
-          Words
-        </h1>
-        <p>
-          post: <br/>
-        date, author, subtitle, body, pictures, on the top there can be a back component
-          <hr/>
-          front: <br/>
-          Cool pic or item for Nav  ||| link back to the portfolio
-          <hr/>
-          title or some piece of words, pictures, or anything for the most intro spot
-          <hr/>
-          have a card for each article
-          <hr/>
-          maybe have another item or something at the bottom to make it looks artsy fartsy
-          <hr/>
-          My personal links
-        </p>
+          <h1>
+            Words
+          </h1>
+          <p>
+            post: <br/>
+          date, author, subtitle, body, pictures, on the top there can be a back component
+          </p>
+          <p>
+            front: <br/>
+            Cool pic or item for Nav  ||| link back to the portfolio
+          </p>
+          <p>  
+            title or some piece of words, pictures, or anything for the most intro spot
+          </p>
+          <p>
+            have a card for each article
+          </p>
+          <p>
+            maybe have another item or something at the bottom to make it looks artsy fartsy
+          </p>
+          <p>
+            My personal links
+          </p>
       </div>
-
-
       <footer>
         <a
           href="https://portfolio-cnc.herokuapp.com/"
